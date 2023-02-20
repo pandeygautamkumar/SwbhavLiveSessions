11b25f57bb9a3b8d972612940aa502dc46ef3336
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-20 22:52:59 +0530

    Add files via upload

---
+++ Session02/CommandLineArgsApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CommandLineArgsApp
+{
+    internal class Program
+    {
+        static void Main(string[] numberOfArgs)
+        {
+            Console.WriteLine("Inside Main");
+            Console.WriteLine(numberOfArgs.Length);
+            for(int i = 0; i < numberOfArgs.Length; i++)
+            {
+                Console.WriteLine("Hello " + numberOfArgs[i]);
+            }
+            Console.WriteLine("End of Main");
+        }
+    }
+}
