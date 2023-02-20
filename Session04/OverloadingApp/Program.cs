fdab585ba92706af42c4cf341b5ac0183d503414
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-20 22:54:24 +0530

    Add files via upload

---
+++ Session04/OverloadingApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OverloadingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDetails(50);
+            PrintDetails(10.55);
+            PrintDetails(10.55f);
+            PrintDetails("Monocept");
+            PrintDetails('a');
+            PrintDetails('A');
+            PrintDetails(10>5);
+        }
+        private static void PrintDetails(int input)
+        {
+            Console.WriteLine("Printing Integer input {0} ,double: {1} ",input,2*input);
+        }
+        private static void PrintDetails(double input)
+        {
+            Console.WriteLine("Printing a double value , value is {0} ", input);
+        }
+        private static void PrintDetails(float input)
+        {
+            Console.WriteLine("Printing a float value , value is {0} ", input);
+        }
+        private static void PrintDetails(string input)
+        {
+            Console.WriteLine("Printing a string {0} ", input);
+        }
+        private static void PrintDetails(char input)
+        {
+            Console.WriteLine("Printing a character value {0}, Ascii value is {1} : ", input,(int)input);
+        }
+        private static void PrintDetails(bool input)
+        {
+            Console.WriteLine("Printing a boolean Value {0} : ", input);
+        }
+    }
+
+}
---
+++ Session04/PassingValuesApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+
+namespace PassingValuesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CaseStudy1();
+            //CaseStudy2();
+            CaseStudy3();
+        }
+        private static void CaseStudy3()
+        {
+            int[] scores = { 10, 20, 30, 40, 50 };
+            UpdateScoresToZero(scores);
+            foreach(int score in scores)
+            {
+                Console.WriteLine(score);
+            }
+            Console.WriteLine("HashCode of scores is {0} : ", scores.GetHashCode());
+        }
+        private static void UpdateScoresToZero(int[] pScores)
+        {
+            for(int i = 0; i < pScores.Length; i++)
+            {
+                pScores[i] = 0;
+            }
+            Console.WriteLine("HashCode of pScores is {0} : ", pScores.GetHashCode());
+        }
+        private static void CaseStudy2()
+        {
+            int salary = 1000;
+            UpdateSalaryToZero(ref salary);
+            Console.WriteLine("CaseStudy2, salary value is : {0} ",salary);
+        }
+        private static void UpdateSalaryToZero(ref int pSalary)
+        {
+            pSalary = 0;
+        }
+        private static void CaseStudy1()
+        {
+            int mark = 100;
+            UpdateMarkToZero(mark);
+            Console.WriteLine("CaseStudy1, mark value is {0} ", mark);
+        }
+        private static void UpdateMarkToZero(int marks)
+        {
+            marks = 0;
+        }
+    }
+}
