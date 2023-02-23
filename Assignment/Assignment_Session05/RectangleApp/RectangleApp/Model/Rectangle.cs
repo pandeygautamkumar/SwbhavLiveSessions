943f78eeee477f7d72ff65f2ac49cf99c619ff67
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-23 23:39:58 +0530

    Add files via upload

---
+++ Assignment/Assignment_Session05/RectangleApp/RectangleApp/Model/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace RectangleApp.Model
+{
+    internal class Rectangle
+    {
+        public int width;
+        public int height;
+
+        public int CalculateArea()
+        {
+            int area = width * height;
+            return area;
+        }
+    }
+}
---
+++ Assignment/Assignment_Session05/RectangleApp/RectangleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using RectangleApp.Model;
+using System;
+
+namespace RectangleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle small;
+            small = new Rectangle();
+            small.height = 5;
+            small.width = 20;
+            Console.WriteLine("Width of Small Rectangle will be : {0} ", small.width);
+            Console.WriteLine("Height of Small Rectangle Will be : {0}", small.height);
+            Console.WriteLine("Area of Small Rectaangle Will be : {0}",small.CalculateArea());
+
+            Rectangle big=new Rectangle();
+            small.height = 50;
+            small.width = 100;
+            Console.WriteLine("Width of Big Rectangle will be : {0} ", small.width);
+            Console.WriteLine("Height of Big Rectangle Will be : {0}", small.height);
+            Console.WriteLine("Area of Big Rectaangle Will be : {0}", small.CalculateArea());
+
+        }
+    }
+}
