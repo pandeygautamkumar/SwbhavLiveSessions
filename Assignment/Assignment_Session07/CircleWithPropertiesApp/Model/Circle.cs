42a41ed2c3fb37f1f409a6b2f7e70c45e15b68cb
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-23 23:39:07 +0530

    Add files via upload

---
+++ Assignment/Assignment_Session07/CircleWithPropertiesApp/Model/Circle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace CircleWithPropertiesApp.Model
+{
+    internal class Circle
+    {
+        private float _radius;
+        private string _color;
+        private string _borderStyle;
+
+        public Circle(float radius, string color, string borderStyle)
+        {
+            _radius = radius;
+            _color = color;
+            _borderStyle = borderStyle;
+        }
+
+        private string ValidateColor(string pColor)
+        {
+            pColor = pColor.ToUpper();
+            if (pColor.ToUpper() == "RED" || pColor.ToUpper() == "BLUE" || pColor.ToUpper() == "GREEN" || pColor.ToUpper() == "YELLOW")
+                return pColor;
+            else
+                return "RED";
+        }
+        private string ValidateBorderStyle(string pBorderStyle)
+        {
+            pBorderStyle = pBorderStyle.ToUpper();
+            if (pBorderStyle == "DOTTED" || pBorderStyle == "SINGLE LINE" || pBorderStyle == "DOUBLE LINE")
+                return pBorderStyle;
+            else
+                return "SINGLE LINE";
+        }
+
+        private float ValidateRadius(float pRadius)
+        {
+            if (pRadius > 60.00f)
+                return 60f;
+            else
+                return pRadius;
+        }
+        public float Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = ValidateRadius(value);
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = ValidateColor(value);
+            }
+        }
+        public string BorderStyle
+        {
+            get
+            {
+                return _borderStyle;
+            }
+            set
+            {
+                _borderStyle = ValidateBorderStyle(value);
+            }
+        }
+        public double CalculateArea()
+        {
+            return (3.14 * _radius * _radius);
+        }
+        public double CalculateCircumference()
+        {
+            return (2 * 3.14 * _radius);
+        }
+    }
+}
---
+++ Assignment/Assignment_Session07/CircleWithPropertiesApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using CircleWithPropertiesApp.Model;
+using System;
+
+namespace CircleWithPropertiesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circleA = new Circle(12.34f,"Green","Dotted");
+            PrintDetails(circleA, "Details of Circle A");
+            circleA.Radius=23.23f;
+            circleA.Color="Yellow";
+            circleA.BorderStyle="Double Line";
+            PrintDetails(circleA, "Details of Circle A After Change");
+        }
+        static void PrintDetails(Circle circle, string description)
+        {
+            Console.WriteLine(description.ToUpper());
+            Console.WriteLine("Redius of Circle : "+circle.Radius);
+            Console.WriteLine("Color of Circle : "+circle.Color);
+            Console.WriteLine("BorderStyle of Circle : "+circle.BorderStyle);
+            Console.WriteLine("Area of Circle : "+circle.CalculateArea());
+            Console.WriteLine("Circumference of Circle : "+circle.CalculateCircumference());
+
+            Console.WriteLine();
+        }
+    }
+}
