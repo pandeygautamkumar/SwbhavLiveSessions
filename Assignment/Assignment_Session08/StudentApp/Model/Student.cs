417c73c145cca8256ffb91f8f33d3409a4188964
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-24 22:56:55 +0530

    Add files via upload

---
+++ Assignment/Assignment_Session08/StudentApp/Model/Student.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace StudentApp.Model
+{
+    internal class Student
+    {
+        private int _rollNo;
+        private string _name;
+        private double _cgpa;
+
+        public Student(int rollNo,string name,double cgpa)
+        {
+            _rollNo = ValidateRollNo(rollNo);
+            _name = ValidateName(name);
+            _cgpa = ValidateCgpa(cgpa);
+        }
+        public int RollNo
+        {
+            get
+            {
+                return _rollNo; 
+            }
+            set
+            {
+                _rollNo = ValidateRollNo(value);
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = ValidateName(value);
+            }
+        }
+        public double Cgpa
+        {
+            get
+            {
+                return _cgpa;
+            }
+            set
+            {
+                _cgpa = ValidateCgpa(value);
+            }
+        }
+        public double CalculatePercentage()
+        {
+            return (_cgpa * 9.5);
+        }
+        private string ValidateName(string name)
+        {
+            if (name.Length>=5)
+            {
+                return name;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        private double ValidateCgpa(double cgpa)
+        {
+            if (cgpa<1)
+            {
+                return 0;
+            }
+            else if(cgpa>10)
+            {
+                return 10;
+            }
+            else
+            {
+                return cgpa;
+            }
+        }
+        private int ValidateRollNo(int rollNo)
+        {
+            if (rollNo > 0)
+                return rollNo;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Assignment/Assignment_Session08/StudentApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using StudentApp.Model;
+using System;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(101, "Gautam Kumar Pandey", 8.5);
+            PrintDetails(student1);
+
+            Console.WriteLine();
+            Student student2 = new Student(101, "Ga", 8.5);
+            PrintDetails(student2);
+
+            Console.WriteLine();
+            Student student3 = new Student(-10, "Gautam", 8.5);
+            PrintDetails(student3);
+
+            Console.WriteLine();
+            Student student4 = new Student(10, "Gautam", 19.00);
+            PrintDetails(student4);
+        }
+
+        private static void PrintDetails(Student student)
+        {
+            Console.WriteLine("Roll Number is : "+student.RollNo);
+            Console.WriteLine("Name is : " + student.Name);
+            Console.WriteLine("CGPA is : " + student.Cgpa);
+            Console.WriteLine("Percentage is : " + student.CalculatePercentage());
+        }
+    }
+}
