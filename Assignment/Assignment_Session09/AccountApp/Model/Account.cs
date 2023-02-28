80bea17d9b9e57fe4a8c37859dda486689e84d8d
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-28 22:22:05 +0530

    Add files via upload

---
+++ Assignment/Assignment_Session09/AccountApp/Model/Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace AccountApp.Model
+{
+    internal class Account
+    {
+        private readonly int _accontNo;
+        private double _balance;
+        private string _name;
+        private const double MIN_BALANCE = 500;
+        public Account(int accontNo, string name, double balance)
+        {
+            _accontNo = accontNo;
+            _name = name;
+            _balance = balance;
+        }
+        /*
+        public Account(int acccontNo, string name)
+        {
+            _accontNo = acccontNo;
+            _name = name;
+            _balance = MIN_BALANCE;
+        }
+        */
+        public Account(int amount,string name)
+        {
+            _balance = amount;
+            _name = name;
+            _accontNo = 1000;
+        }
+
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (HasSufficientFund(amount) == true)
+            {
+                _balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Error No Fund");
+            }
+        }
+        private bool HasSufficientFund(double balance)
+        {
+            double amountAfterWithdraw = _balance - balance;
+            return (amountAfterWithdraw >= MIN_BALANCE);
+        }
+
+        public double AccountNo
+        {
+            get
+            {
+                return _accontNo;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
---
+++ Assignment/Assignment_Session09/AccountApp/Program.cs
@@ -0,0 +1,110 @@
+﻿using AccountApp.Model;
+using System;
+
+namespace AccountApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CaseStudy1();
+            //CaseStudy2();
+            // CaseStudy3();
+        }
+       
+        private static void CaseStudy3()
+        {
+            Account[] manyAccounts = new Account[4];
+            manyAccounts[0] = new Account(1001, "Gautam Kumar Pandey", 1000);
+            manyAccounts[1] = new Account(1002, "Sandeep Kumar Pandey", 1200.56);
+            manyAccounts[2] = new Account(1003, "Ravi Kumar", 24242.67);
+            manyAccounts[3] = new Account(1004, "Gautam Kumar Pandey", 1200);
+            manyAccounts[1].Name = "Ram";
+            Account[] longNamedAccHolders = FindAccountHolderWithLongNames(manyAccounts);
+            foreach(Account account in longNamedAccHolders)
+            {
+                if(account.Name!=null)
+                   PrintDetails(account);
+            }
+        }
+
+        public static Account[] FindAccountHolderWithLongNames(Account[] manyAccounts)
+        {
+            int count = 0;
+            for (int i = 0; i < manyAccounts.Length; i++)
+            {
+                if (manyAccounts[i].Name.Length >=10)
+                {
+                    count++;
+                }
+            }
+            Account[] longNameAccount = new Account[count];
+            int j= 0;
+            for (int i = 0; i < manyAccounts.Length; i++)
+            {
+                if (manyAccounts[i].Name.Length >= 10)
+                {
+                    longNameAccount[j++] = manyAccounts[i];
+                }
+            }
+            return longNameAccount;
+        }
+        private static void CaseStudy2()
+        {
+            Account[] manyAccounts = new Account[4];
+            manyAccounts[0] = new Account(1001, "Gautam Kumar Pandey", 1000);
+            manyAccounts[1] = new Account(1002, "Sandeep Kumar Pandey", 1200.56);
+            manyAccounts[2] = new Account(1003, "Ravi Kumar", 24242.67);
+            manyAccounts[3] = new Account(1004, "Gautam Kumar Pandey", 1200);
+            manyAccounts[2].Withdraw(20000);
+            Account rich = FindTheRichestAccountHolder(manyAccounts);
+            PrintDetails(rich);
+        }
+
+        public static Account FindTheRichestAccountHolder(Account[] manyAccounts)
+        {
+            Account maxAmmountHolder = manyAccounts[0];
+            for (int i = 1; i < manyAccounts.Length; i++)
+            {
+                if (manyAccounts[i].Balance >maxAmmountHolder.Balance)
+                {
+                    maxAmmountHolder = manyAccounts[i];
+                }
+            }
+            return maxAmmountHolder;
+        }
+
+        private static void CaseStudy1()
+        {
+            Console.Write("Enter How Many Account Holder Will be Present : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Account[] manyAccounts = new Account[number];
+            for (int i = 0; i < number; i++)
+            {
+                manyAccounts[i] = TakeUserInput();
+            }
+            foreach (Account account in manyAccounts)
+            {
+                PrintDetails(account);
+            }
+        }
+        
+        static Account TakeUserInput()
+        {
+            Console.Write("Enter Account Number : ");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Name Of Account Holder : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Initial Ammount : ");
+            int balance = Convert.ToInt32(Console.ReadLine());
+            Account account = new Account(accountNumber, name, balance);
+            return account;
+        }
+        static void PrintDetails(Account account)
+        {
+            Console.WriteLine("Account Number is : " + account.AccountNo);
+            Console.WriteLine("Account Holder Name is : " + account.Name);
+            Console.WriteLine("Amount Will be : " + account.Balance);
+        }
+    }
+}
