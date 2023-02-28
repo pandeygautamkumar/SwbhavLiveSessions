83ca81f96f89fe17d5cc8a33f1dfd48f8cabb3cb
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-02-28 22:18:28 +0530

    Add files via upload

---
+++ Session09/PlayerApp/Model/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PlayerApp.Model
+{
+    internal class Player:Object
+    {
+        private int id;
+        private string name;
+        private int age;
+        private const int MIN_AGE = 18;
+        public Player(int id,string name,int age)
+        {
+            
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            
+            Console.WriteLine("3 Params Constructor.");
+           
+        }
+        public Player(int id,string name):this(id, name,MIN_AGE)
+        {
+            /*
+            this.id = id;
+            this.name = name;
+            age = 18;
+            */
+            Console.WriteLine("2 Params Constructor.");
+        }
+        public Player WhoIsElder(Player secondPlayer)
+        {
+            if (this.age < secondPlayer.age)
+                return secondPlayer;
+            else
+                return this;
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+    }
+}
---
+++ Session09/PlayerApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using PlayerApp.Model;
+using System;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CaseStudy1();
+            //CaseStudy2();
+            DemoStudy3();
+        }
+
+        private static void DemoStudy3()
+        {
+            Player sachin = new Player(1, "Sachin", 56);
+            Player[] manyPlayer = new Player[5];
+            manyPlayer[0] = new Player(1, "Player 1", 20);
+            manyPlayer[1] = sachin;
+            manyPlayer[2] = sachin;
+            manyPlayer[3] = sachin;
+            manyPlayer[4] = sachin;
+
+            foreach (Player player in manyPlayer)
+            {
+                PrintDetails(player);
+            }
+        }
+
+        private static void CaseStudy1()
+        {
+            Player playerA = new Player(1, "Sachin", 54);
+            Player playerB = new Player(2, "Virat");
+            PrintDetails(playerA, "Player A Details.");
+            PrintDetails(playerB, "Player B Details.");
+        }
+
+        private static void CaseStudy2()
+        {
+            Player sachin = new Player(1, "Sachin", 54);
+            Player virat = new Player(2, "Virat");
+            // Player elder=virat.WhoIsElder(sachin);
+            Player elder = sachin.WhoIsElder(virat);
+            //PrintDetails(elder, "Elder Object");
+
+            Console.WriteLine("Sachin HasCode : " + sachin.GetHashCode());
+            Console.WriteLine("Virat HashCode : " + virat.GetHashCode());
+            Console.WriteLine("Elder hashCode : " + elder.GetHashCode());
+        }
+
+        static void PrintDetails(Player player,string description="")
+        {
+            Console.WriteLine(description.ToUpper());
+            Console.WriteLine("Id is : "+player.Id);
+            Console.WriteLine("Name is : "+player.Name);
+            Console.WriteLine("Age is : "+player.Age);
+        }
+    }
+}
