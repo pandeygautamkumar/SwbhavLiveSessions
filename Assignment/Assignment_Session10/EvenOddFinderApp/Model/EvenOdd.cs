05e932e43cb957733f97d0e4c791b834b16ceccf
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-03-02 22:01:31 +0530

    Add files via upload

---
+++ Assignment/Assignment_Session10/EvenOddFinderApp/Model/EvenOdd.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+
+namespace EvenOddFinderApp.Model
+{
+    internal class EvenOdd
+    { 
+        public int[] GetEven(int start, int end)
+        {
+            int[] evenNumber;
+            int totalNumber = end - start + 1;
+            if (totalNumber % 2 == 0)
+                evenNumber = new int[totalNumber / 2];
+            else
+                evenNumber = new int[totalNumber / 2 + 1];
+
+            int j = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    evenNumber[j++] = i;
+            }
+            return evenNumber;
+        }
+
+        public int[] GetEven(int end)
+        {
+            int[] evenNumber;
+            if (end % 2 == 0)
+                evenNumber = new int[end / 2];
+            else
+                evenNumber = new int[end / 2 + 1];
+
+            int j = 0;
+            for (int i = 1; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    evenNumber[j++] = i;
+            }
+            return evenNumber;
+        }
+        public int[] GetOdd(int start,int end)
+        {
+            int[] oddNumber;
+            int totalNumber = end - start + 1;
+            if (totalNumber % 2 == 0)
+                oddNumber = new int[totalNumber / 2];
+            else
+                oddNumber = new int[totalNumber / 2 + 1];
+
+            int j = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 != 0)
+                    oddNumber[j++] = i;
+            }
+            return oddNumber;
+        }
+        public int[] GetOdd(int end)
+        {
+            int[] oddNumber;
+            if(end%2==0)
+                oddNumber= new int[end/2];
+            else
+                oddNumber= new int[end/2+1];
+
+            int j = 0;
+            for(int i = 1; i <= end; i++)
+            {
+                if(i%2!=0)
+                    oddNumber[j++]= i;
+            }
+            return oddNumber;
+        }
+
+    }
+}
---
+++ Assignment/Assignment_Session10/EvenOddFinderApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using EvenOddFinderApp.Model;
+using System;
+
+namespace EvenOddFinderApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CaseStudy1();
+            CaseStudy2();
+        }
+
+        private static void CaseStudy2()
+        {
+            EvenOdd evenOdd1 = new EvenOdd();
+            int[] oddNumbers = evenOdd1.GetOdd(100);
+            int[] evenNumbers = evenOdd1.GetEven(100);
+            Console.WriteLine(evenOdd1);
+            PrintDetails(oddNumbers, "Odd Numbers");
+            PrintDetails(evenNumbers, "Even Numbers");
+        }
+
+        private static void CaseStudy1()
+        {
+            EvenOdd evenOdd1 = new EvenOdd();
+            int[] oddNumbers = evenOdd1.GetOdd(100, 200);
+            int[] evenNumbers = evenOdd1.GetEven(100, 200);
+            PrintDetails(oddNumbers, "Odd Numbers");
+            PrintDetails(evenNumbers, "Even Numbers");
+        }
+
+        private static void PrintDetails(int[] numbers,string description)
+        {
+            Console.WriteLine(description+" : ");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
