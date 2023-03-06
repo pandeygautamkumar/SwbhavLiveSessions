cb40cb2cb476125726fffbde8bb0e006a01db479
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2023-03-06 11:08:12 +0530

    Add files via upload

---
+++ MonoceptMarch/Assignment/Assignment_Session13/AccountConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AccountLibrary;
+
+namespace AccountConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account accountA = new Account(1020302, "Gautam Kumar Pandey", 1000);
+            PrintDetails(accountA,"Initial Ammount of Account A");
+            accountA.Deposit(5000);
+            PrintDetails(accountA, "After Deposit Account A");
+            accountA.Withdraw(5000);
+            PrintDetails(accountA, "After Withdraw Some Ammount from Account A");
+            accountA.Withdraw(5000);
+            PrintDetails(accountA, "Withdraw Some Ammount from Account A if No Sufficient Ammount Available");
+        }
+        private static void PrintDetails(Account account,string description)
+        {
+            Console.WriteLine(description.ToUpper());
+            Console.WriteLine("Account Number is : "+account.AccountNo);
+            Console.WriteLine("Account Holder Name : "+ account.Name);
+            Console.WriteLine("Ammount is : " + account.Balance);
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
